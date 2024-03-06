5f774928949e7f3216ab38b411b2a6fc93088605
tomhoenderdos <hoenderdos.t@hsleiden.nl>
2024-03-06 10:34:47 +0100

    Add CORS exception for Angular running localhost:4200

--- src/main/java/com/example/todoappdeel3/controller/ProductController.java
+++ src/main/java/com/example/todoappdeel3/controller/ProductController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/products")
 public class ProductController {
 
